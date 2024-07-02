37c0fef46d3b6c7bcee9e00efd43080417c5a00c
Cassidy Cubra <10cubracm@gmail.com>
2024-07-02 11:48:09 -0400

    r

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+# how to install the package? "pip install ." or "pip install --user -e ."
+
+setup(
+    name = 'Hw9',
+    version= '0.1',
+    description= 'A simple demo pancake',
+    url= 'https://github.com/casscub24/inclassPackage.git',
+    author= 'Cassidy Cubra',
+    author_email='drz8rt@virginia.edu',
+    license= 'MIT',
+    packages=['mypackage']
+)
